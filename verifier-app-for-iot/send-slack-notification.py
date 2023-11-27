063bcefea2f8faa5ac0400a2c328823ec32bdc4d
dorakemon <51844896+dorakemon@users.noreply.github.com>
2023-11-27 12:53:14 +0900

    feat: add slack nitification logic

---
+++ verifier-app-for-iot/send-slack-notification.py
@@ -0,0 +1,39 @@
+import requests
+import os
+
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', 'sample-channel')
+SLACK_TOKEN = os.environ.get("SLACK_TOKEN", "xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+class SlackDriver:
+    def send_message(self, message):
+        params = {
+            'channel': SLACK_CHANNEL, 
+            'text': message,
+        }
+        r = requests.post('https://slack.com/api/chat.postMessage',
+                          headers={'Authorization': 'Bearer ' + self._token},
+                          params=params
+        )
+        return r.json()["message"]
+
+    def send_image(self, file_path, thread_ts):
+        files = {'file': open(file_path, 'rb')}
+        params = {
+            'channels': SLACK_CHANNEL, 
+            'filetype': 'json_ld',
+            'title': 'VP Log',
+            'filename': "vp.json",
+            'thread_ts': thread_ts,
+        }
+
+        requests.post('https://slack.com/api/files.upload',
+                          headers={'Authorization': 'Bearer ' + self._token},
+                          params=params,
+                          files=files
+        )
+
+
+if __name__ == '__main__':
+    slack = SlackDriver()
+    res = slack.send_message('The door is opened by someone :female_supervillain:')
+    slack.send_image("SampleVP.json", res["ts"])
