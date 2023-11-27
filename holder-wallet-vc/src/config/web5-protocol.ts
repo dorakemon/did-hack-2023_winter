36ba5bb1f1dc872b1367108cf3384acc850aa74e
dorakemon <51844896+dorakemon@users.noreply.github.com>
2023-11-27 11:32:16 +0900

    chore: commit for merge

--- holder-wallet-vc/src/config/web5-protocol.ts
+++ holder-wallet-vc/src/config/web5-protocol.ts
@@ -1,28 +1,59 @@
 export const DingerProtocolDefinition = {
-  protocol: "https://schema.org/Playlist",
+  protocol: 'https://schema.org/Playlist',
   published: true,
   types: {
     ding: {
-      schema: "ding",
-      dataFormats: ["application/json"],
+      schema: 'ding',
+      dataFormats: ['application/json'],
     },
   },
   structure: {
     ding: {
       $actions: [
         {
-          who: "anyone",
-          can: "write",
+          who: 'anyone',
+          can: 'write',
         },
         {
-          who: "author",
-          of: "ding",
-          can: "read",
+          who: 'author',
+          of: 'ding',
+          can: 'read',
         },
         {
-          who: "recipient",
-          of: "ding",
-          can: "read",
+          who: 'recipient',
+          of: 'ding',
+          can: 'read',
+        },
+      ],
+    },
+  },
+};
+
+export const VCProtocolDefinition = {
+  protocol: 'https://www.w3.org/ns/credentials/v2',
+  published: true,
+  types: {
+    vp: {
+      schema: 'vp',
+      dataFormats: ['application/json'],
+    },
+  },
+  structure: {
+    vp: {
+      $actions: [
+        {
+          who: 'anyone',
+          can: 'write',
+        },
+        {
+          who: 'author',
+          of: 'vp',
+          can: 'read',
+        },
+        {
+          who: 'recipient',
+          of: 'vp',
+          can: 'read',
         },
       ],
     },
